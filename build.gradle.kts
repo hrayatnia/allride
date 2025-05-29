0d999023c6f6844351317f5e53dcdaed12bc720c
Sam Rayatnia <h.rayatnia@gmail.com>
2025-05-29 10:46:04 +0200

    Initializes Ktor project with Kafka integration
    Sets up a basic Ktor application configured to use Kafka,
    including necessary dependencies, plugins, and configurations
    for Kafka producer and consumer functionality. It also includes
    Swagger UI for API documentation.

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+}
+
+group = "me.rayatnia"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+dependencies {
+    implementation(libs.ktor.server.kafka)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.swagger)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
---
+++ src/main/kotlin/HTTP.kt
@@ -0,0 +1,27 @@
+package me.rayatnia
+
+import io.github.flaxoos.ktor.server.plugins.kafka.Kafka
+import io.github.flaxoos.ktor.server.plugins.kafka.MessageTimestampType
+import io.github.flaxoos.ktor.server.plugins.kafka.TopicName
+import io.github.flaxoos.ktor.server.plugins.kafka.admin
+import io.github.flaxoos.ktor.server.plugins.kafka.common
+import io.github.flaxoos.ktor.server.plugins.kafka.consumer
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerConfig
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerRecordHandler
+import io.github.flaxoos.ktor.server.plugins.kafka.producer
+import io.github.flaxoos.ktor.server.plugins.kafka.registerSchemas
+import io.github.flaxoos.ktor.server.plugins.kafka.topic
+import io.ktor.client.HttpClient
+import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.slf4j.event.*
+
+fun Application.configureHTTP() {
+    routing {
+        swaggerUI(path = "openapi")
+    }
+}
